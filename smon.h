3b1cd3bd90838ac58655ee3922d3a29bbb1af714
Felipe Pena <felipensp@gmail.com>
2012-12-22 15:26:32 -0200

    - Added some comments

--- smon.h
+++ smon.h
@@ -1,8 +1,14 @@
 #ifndef SMON_H
 #define SMON_H
 
+/**
+ * Segments available to be monited
+ */
 #define SMON_STACK 0x1
 
+/**
+ * smon functions
+ */
 void smon_pid(pid_t, size_t);
 void smon_exec(const char*, size_t);
 
